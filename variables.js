6fb664ad11c2b2fbfb197890f95b8c78cf4d8e7c
kevinmora47 <kevinmora47@gmail.com>
2020-11-15 19:14:01 -0600

    Variables

---
+++ variables.js
@@ -0,0 +1,22 @@
+//string variale.
+var nombre = "Kevin";
+
+//Declaring variable.
+var edad;
+
+//Initialize variable.
+edad = 21;
+
+//Calling variable.
+edad;
+
+//Array variables.
+var elemento= ["Computador", "Celular"];
+
+//Object variables.
+var persona = {
+    nombre:"Kevin",
+    edad:21
+}
+//Object called.
+persona

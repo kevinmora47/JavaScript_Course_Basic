432a0349692c7fa3da1b5d78d57e15c3e29f88b3
kevinmora47 <kevinmora47@gmail.com>
2020-11-15 19:50:14 -0600

    Functions.

---
+++ functions.js
@@ -0,0 +1,18 @@
+//Declarativas.
+function miFuncion() {
+  return 3;
+}
+
+miFuncion
+
+//Expresión
+var miFuncion = function (a,b) {
+  return a + b;
+};
+
+miFuncion(2,2);
+
+// Ejemplos de funciones
+function sumar(a,b){
+return a + b;
+}

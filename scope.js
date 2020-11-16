6f4d489f50c499b09731bdedf441208b3e797997
kevinmora47 <kevinmora47@gmail.com>
2020-11-16 06:21:19 -0600

    Definition of scope

--- scope.js
+++ scope.js
@@ -6,3 +6,6 @@ console.log(miNombre + " " + ape);
 }
 
 nombre();
+
+// **Veamos el siguiente ejemplo:**
+// En este caso como vemos tenemos en cuenta los 2 scope, en este ejemplo las variables globales se pueden acceder por medio de un scope local, pero no podemos hacer lo contrario, ya que no tenemos el alcance necesario para poder acceder a variables de un scope local desde uno global, por esto no brinda la alerta de undefined.

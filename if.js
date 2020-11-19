832a8355adc0309bac51230caa50421217195fa6
kevinmora47 <kevinmora47@gmail.com>
2020-11-18 21:03:00 -0600

    If structure

---
+++ if.js
@@ -0,0 +1,10 @@
+if(false){
+console.log("Esto no va a pasar.")
+}else{
+    console.log("Esto si pasara")
+}
+
+// Otra manera de hacer un if.
+var numero =1;
+numero > 2 ? console.log("Mayor") : console.log("Menor")
+

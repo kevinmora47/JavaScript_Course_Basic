bdf777d69e2e652509f8bd15ec33a49b1b21c144
kevinmora47 <kevinmora47@gmail.com>
2020-11-19 20:05:22 -0600

    Arrays

---
+++ arrays.js
@@ -0,0 +1,24 @@
+// Creation of array.
+var frutas = ["Mazana", "Banana", "Cereza", "Fresa"];
+console.log(frutas);
+
+// Knowing legth of array.
+console.log(frutas.length)
+
+// Accesing to an array element.
+console.log(frutas[0]);
+
+//Adding fruits to our array.
+frutas.push("Coco");
+
+//delete last fruit.
+frutas.pop();
+
+//Adding value to an array to the start.
+frutas.shift("Coco");
+
+//Delete first element.
+frutas.shift();
+
+//Knowing positions of array.
+frutas.indexOf("Banana");

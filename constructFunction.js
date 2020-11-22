ed1e00ae4de08b1afaeee70285c8f521d125705d
kevinmora47 <kevinmora47@gmail.com>
2020-11-21 18:02:21 -0600

    Objects

---
+++ constructFunction.js
@@ -0,0 +1,26 @@
+// Object template.
+function auto(marca, modelo, annio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+var autoNuevo = new auto("Testla", "Model 3", 2020);
+
+// Function.
+function auto (MARCA, MODELO, ANNIO){
+    this.marca = MARCA;
+    this.modelo = MODELO;
+    this.annio = ANNIO;
+  }
+  var autos = [];
+  for(let i = 0 ; i < 30 ; i++){
+    var marca = prompt("Ingresa la marca del auto");
+    var modelo = prompt("Ingresa el modelo del auto");
+    var annio = prompt("Ingresa el año del auto");
+    autos.push(new auto (marca, modelo, annio));
+  }
+  
+  for(let i = 0 ; i < autos.length ; i++){
+    console.log(autos[i]);
+  }
